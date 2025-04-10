69d2670d407b954fda84617253909a17c83970c9
TalesLimaOliveira <104475181+TalesLimaOliveira@users.noreply.github.com>
2025-04-10 14:22:59 -0300

    Initial commit

--- atividades/lista_01-090425/04.py
+++
@@ -1,8 +0,0 @@
-def mario_pyramid(height):
-    for i in range(1, height + 1):
-        spaces = ' ' * (height - i)
-        blocks = '#' * i
-        print(spaces + blocks)
-
-user_height = int(input("Tamanho: "))
-mario_pyramid(user_height)
